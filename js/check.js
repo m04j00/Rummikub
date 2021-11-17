99e8858fbced6c935527e5a7da9bc925ae0cf6e7
LEE MINJI <m04j00@gmail.com>
2021-11-17 13:10:37 +0900

    타일 정렬

--- js/check.js
+++ js/check.js
@@ -26,6 +26,8 @@ const shuffle = (arr) => {
 }
 inputTile();
 shuffle(initTile);
+shuffle(initTile);
+shuffle(initTile);
 // for(let i = 0; i < 104; i++){console.log(initTile[i])}
 
 // playerBoard = document.querySelector('.board');
@@ -51,14 +53,18 @@ for (let i = 0, p = 0, a = 0, r = 0; i < 104;) {
         remainTile[r++] = initTile[i++];
     }
 }
-console.log(playerTile.length, AITile.length, remainTile.length);
+// console.log(playerTile.length, AITile.length, remainTile.length);
 
 playerBoard = document.querySelector('.player-board');
-for (let i = 0; i < playerTile.length; i++) {
-    let id = playerTile[i].id;
-    let path = playerTile[i].path;
-    playerBoard.innerHTML += '<img onclick="plyaer_tile_click(' + id + ')" id="' + id + '" src="image/' + path + '.svg" class="tile no-drag">';
+input_player_tile();
+function input_player_tile(){
+    for (let i = 0; i < playerTile.length; i++) {
+        let id = playerTile[i].id;
+        let path = playerTile[i].path;
+        playerBoard.innerHTML += '<img onclick="plyaer_tile_click(' + id + ')" id="' + id + '" src="image/' + path + '.svg" class="tile no-drag">';
+    }
 }
+
 AIBoard = document.querySelector('.AI-board');
 for (let i = 0; i < playerTile.length; i++) {
     let id = AITile[i].id;
@@ -93,6 +99,13 @@ for (let i = 0; i < playerTile.length; i++) {
 // });
 
 
+// 타일 선택 시 clickTile 배열에 추가
+const clickTile = [];
+
+function tile_click_shadow(id) {
+    const tile = document.getElementById(id);
+    tile.classList.toggle('tile-click');
+}
 
 function plyaer_tile_click(id) {
     //alert(id); 
@@ -100,11 +113,18 @@ function plyaer_tile_click(id) {
         return e.id == id;
     });
     const tile = document.getElementById(id);
-    // tile.className.add("tile-click");
-    document.getElementById(id).classList.toggle('tile-click');
-    console.log(tile)
-
-    console.log(playerTile[tilInfo]);
+    if (!tile.classList.contains('tile-click')) {
+        clickTile.push(playerTile[tilInfo]);
+    } else {
+        const popTile = clickTile.findIndex((e) => {
+            return e.id == id;
+        })
+        clickTile.splice(popTile, 1);
+    }
+    tile_click_shadow(id);
+    // console.log("tile", tile)
+    // console.log(clickTile)
+    // console.log(playerTile[tilInfo]);
 }
 
 function AI_tile_click(id) {
@@ -119,3 +139,63 @@ function AI_tile_click(id) {
 
     console.log(AITile[tilInfo]);
 }
+
+function set_board_click() {
+    isPass();
+    const mainBoard = document.querySelector('.main-board');
+    let div = document.createElement("div");
+    for (let i = 0; i < clickTile.length; i++) {
+        let id = clickTile[i].id;
+        let path = clickTile[i].path;
+
+        //플레이어 보드에 이미지 노드 삭제
+        const tileInfo = playerTile.findIndex((e) => {
+            return e.id == id;
+        });
+        playerTile.splice(tileInfo, 1);
+        const nodeImg = document.getElementById(id);
+        playerBoard.removeChild(nodeImg);
+
+        //메인 보드에 이미지 노드 추가
+        const set_tile = document.querySelector('.set-board');
+        div.innerHTML += '<img onclick="plyaer_tile_click(' + id + ')" id="' + id + '" src="image/' + path + '.svg" class="tile no-drag">';
+        mainBoard.insertBefore(div, set_tile);
+    }
+    clickTile.length = 0;
+}
+let PorF = [];
+function isPass(){
+    PorF = clickTile.slice();
+    console.log(PorF);
+}
+
+// 플레이어 타일 sort
+//숫자 정렬
+function player_a_to_z(){
+    playerTile.sort(function(a, b){
+        return a.number - b.number;
+    })
+}
+//색깔 정렬
+function player_r_to_b(){
+    playerTile.sort(function(a, b){
+        return a.color - b.color;
+    })
+}
+function player_tile_refresh(){
+    playerBoard.innerText = '';
+    input_player_tile();
+}
+
+function r_to_b_click(){
+    player_r_to_b();
+    player_a_to_z();
+    player_tile_refresh();
+}
+function a_to_z_click(){
+    player_a_to_z();
+    player_r_to_b();
+
+    player_tile_refresh();
+}
+
